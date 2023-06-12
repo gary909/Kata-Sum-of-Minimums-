b0ec5162c99386b523cd291edaf752f991eef524
gary909 <garywhite61@hotmail.com>
2023-06-12 12:28:59 +0200

    code

---
+++ app.js
@@ -0,0 +1,6 @@
+function sumOfMinimums(arr) {
+    // your code here
+}
+
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])); // 9
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]])); // 76
