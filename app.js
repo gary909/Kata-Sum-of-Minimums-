2f31c76ff41f5c25abc511004b846a7d40e6f3e2
gary909 <garywhite61@hotmail.com>
2023-06-12 12:49:24 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function sumOfMinimums(arr) {
-    // your code here
+    let arrCnt = 0; // keep count
+    for(let i = 0; i < arr.length; i++){
+        arr[i].sort((a,b) => a-b); // put array[i] in num order
+        arrCnt = arrCnt + arr[i][0]; // count that number
+    }
+    return arrCnt;
 }
 
 console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])); // 9
